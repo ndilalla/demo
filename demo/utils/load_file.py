0a0dc1e0d2e1bf752c25af8eb4fd8fed5e49224b
ndilalla <niccolo.dilalla@stanford.edu>
2023-05-22 14:57:28 -0700

    Wrong commit.

--- demo/utils/load_file.py
+++ demo/utils/load_file.py
@@ -1,6 +1,6 @@
 import numpy
 
 def load_file(file_path):
-    x, y, dy = numpy.loadtxt(file_path, unpack=True)
+    x, y, dy = numpy.loadtxt(file_path, unpack=False)
     x *= 10
     return x, y, dy
